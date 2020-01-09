c02be8ef2a5c4f8f3d9259a72ca9ce7ad0580a14
Lucas Alonso <alonsosistemas@gmail.com>
2020-01-09 00:12:31 -0300

    v1

---
+++ games.js
@@ -0,0 +1,43 @@
+const player1 = "x";
+const player2 = "o";
+
+var playTime = player1;
+var gameOver = false;
+
+atualizaMostrador();
+inicializarEspacos();
+
+function atualizaMostrador(){
+    if (gameOver) { return;}
+
+    if (playTime == player1){
+
+        var player = document.querySelectorAll("div#mostrador img")[0];
+        player.setAttribute("src", "imagens/x.png");
+    } else {
+        var player = document.querySelectorAll("div#mostrador img")[0];
+        player.setAttribute("src", "imagens/o.png");
+    }
+
+}
+function inicializarEspacos(){
+    var espacos = document.getElementsByClassName("espaco");
+    for (var i = 0; i < espacos.length; i++){
+        espacos[i].addEventListener("click", function(){
+            if (gameOver) {return;}
+
+            if(this.getElementsByClassName("img").length ==0){
+                if(playTime == player1){
+                    this.innerHTML = "<img src='imagens/x.png'>"
+                    this.setAttribute("jogada", player1);
+                    playTime = player2;
+                }else {
+                    this.innerHTML = "<img src='imagens/o.png'>"
+                    this.setAttribute("jogada", player2);
+                    playTime = player1;
+                }
+                atualizaMostrador();
+            }
+        })
+    }
+}
